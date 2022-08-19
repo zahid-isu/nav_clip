4cbbb33a7eafcf1d1b23a01fa166bbce41de8717
Federico Bianchi <chiccobia@gmail.com>
2022-08-19 13:30:22 -0700

    Update data.py

--- src/training/data.py
+++ src/training/data.py
@@ -30,13 +30,13 @@ from open_clip import tokenize
 
 
 class CsvDataset(Dataset):
-    def __init__(self, input_filename, transforms, img_key, caption_key, hard_caption_key, sep="\t"):
+    def __init__(self, input_filename, transforms, img_key, caption_key, hard_captions_key, sep="\t"):
         logging.debug(f'Loading csv data from {input_filename}.')
         df = pd.read_csv(input_filename, sep=sep)
 
         self.images = df[img_key].tolist()
         self.captions = df[caption_key].tolist()
-        self.hard_captions = df[hard_caption_key].tolist()
+        self.hard_captions = df[hard_captions_key].tolist()
         self.transforms = transforms
         logging.debug('Done loading data.')
 
